80d369585c5747e43c17d6c67efee9544a4a6618
sanskarjoshi1998 <57057953+sanskarjoshi1998@users.noreply.github.com>
2022-07-10 16:08:50 +0530

    Update mail.php

--- mail.php
+++ mail.php
@@ -6,7 +6,7 @@ $message= $_POST['message'];
 $to = "sanskarjoshi1998@gmail.com";
 $subject = "Mail From website";
 $txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Message =" . $message;
-$headers = "From: noreply@codeconia.com" . "\r\n" .
+$headers = "From: sanskarjoshi26@gmail.com" . "\r\n" .
 "CC: somebodyelse@example.com";
 if($email!=NULL){
     mail($to,$subject,$txt,$headers);
